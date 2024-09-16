cdcc9abedcadc982b34de9cc74861dea691be7ac
Shivamvashist <ninjavashistsv1@gmail.com>
2024-09-16 23:57:32 +0530

    Assignment 1 Done! with commander package

--- Week 4/Class 1 NodeJs Bun/assignment 1/index.js
+++ Week 4/Class 1 NodeJs Bun/assignment 1/index.js
@@ -1,23 +1,39 @@
 const { Command } = require('commander');
 const program = new Command();
 const fs =  require("fs");
+const path = require("path");
 
 //Assignment 1 => 
 // Input - node index.js /Users/kirat/file.txt
 // Output - You have 10 words in this file
 
 let ctWords ;
+let ctLine;
 
-const file = 
 
-fs.readFile(file,"utf8",(err,data)=>{
-    if(err){
-        console.log("file not found!")
-    }else{
-        ctWords = data.split(' ');
-        console.log(ctWords)
-    }
+
+program
+.description("A cli to do file based Tasks")
+.name("counter")
+.version("0.0.1")
+
+program.command("count")
+.description("Count the number of lines in the file")
+.argument('<file>',"file to read")
+.action((file)=>{
+    fs.readFile(file,"utf8",(err,data)=>{
+        if(err){
+            console.log("file not found!")
+        }else{
+            ctLine = data.split('\n').length;
+            console.log(`There are ${ctLine} lines in your file`)
+        }
+    })
 })
+
+program.parse();
+
+
 
 
 
