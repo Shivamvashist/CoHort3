5e88373dc761775c413c196a91f40886ad49c8a4
Shivamvashist <ninjavashistsv1@gmail.com>
2024-09-20 00:29:40 +0530

    added Get and Post fn in Host

--- Week 4/Offlines/Express nd Postman HTTP/hosp app Express/index.js
+++ Week 4/Offlines/Express nd Postman HTTP/hosp app Express/index.js
@@ -1,11 +1,51 @@
 const express = require("express");
 const app = new express;
 
-app.get("/",function(req,res){
+app.use(express.json());
+
+
+var users = [{
+    name: "John",
+    Kidneys: [{
+        healthy:false
+    },{
+        healthy:true
+    }]
+
+}]
+
+
+
+app.get("/",function(req,res) {
+    const JohnKidneys = users[0].Kidneys
+    const noOfKidneys = JohnKidneys.length;
+    const noOfHealthyKidneys = JohnKidneys.filter(kidney => kidney.healthy === true ).length
+    const noOfUnHealthyKidneys = JohnKidneys.filter(kidney => kidney.healthy === false ).length
+    // console.log(`Total Kidneys = ${noOfKidneys} \nHealthy Kidneys = ${noOfHealthyKidneys} \nUnHealthy Kidneys = ${noOfUnHealthyKidneys}`)
     res.json({
-        'hi':1
-    });
-    
+        noOfKidneys,
+        noOfHealthyKidneys,
+        noOfUnHealthyKidneys
+    })
+
+})
+
+app.post("/",function(req,res) {
+    const addKidney = req.body.addKidney
+    users[0].Kidneys.push({
+        healthy: addKidney
+    })
+    res.json({
+        msg : "Done!!!"
+    })
+})
+
+app.put("/",function(req,res){
+
+})
+
+app.delete("/",function(req,res){
+
 })
 
 app.listen(3000);
