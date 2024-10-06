6826163cc7d750f05c756fe852ce548b45981303
Shivamvashist <ninjavashistsv1@gmail.com>
2024-10-06 18:53:05 +0530

    signup endpoint working and logging into database too

---
+++ Week 7 MongoDB/class 1 7.1 mongoDb/todo-App-FullStack/public/script/signup.js
@@ -0,0 +1,8 @@
+async function Signup(){
+    const response = await axios.post("http://localhost:3000/signup",{
+        email : document.querySelector(".emailInp").value,
+        username : document.querySelector(".usernameInp").value,
+        password : document.querySelector(".passwordInp").value
+    })
+    alert("Hello! I am an alert box!!");
+}
