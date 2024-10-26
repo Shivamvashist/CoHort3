c25a6631ccdf3d16a939ed52e64d1c93d6c2519d
Shivamvashist <ninjavashistsv1@gmail.com>
2024-10-26 21:30:51 +0530

    done Mounting , Unmounting, Conditional Rendering, CleanUp

--- Week 9 React/class2 9.2 Reach basics 2/react-app-1/src/App.jsx
+++ Week 9 React/class2 9.2 Reach basics 2/react-app-1/src/App.jsx
@@ -1,13 +1,27 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
+
 
 
 
 
 function App() {
+
+  const [counterVisible,setCounterVisible]=useState(true);
+
+
+
+  useEffect(function(){
+    setInterval(()=>{
+      setCounterVisible(c=>!c)
+    },10000)
+  },[])
+
   return <div>
     <h1>Hi There!</h1>
-    <Counter></Counter>
+    {/* {counterVisible ? <Counter></Counter> : null} */}
+  <div style={{visibility: counterVisible ? "visible" : "hidden"}} ><Counter></Counter></div>
   </div>
+
 
 }
 
@@ -15,9 +29,24 @@ function Counter() {
 
   const [count, setCount] = useState(0);
   
+
   function onClickFn(){
     setCount(count + 1);
   }
+
+  useEffect(function() {
+    console.log("on Mount")
+    let clock = setInterval(function(){
+      console.log("inside Interval")
+      setCount(count=>count+1);
+    },1000);
+
+    return function(){
+      console.log("on unmount")
+      clearInterval(clock)
+    }
+
+  },[]);
 
   return<div> 
   <h1>{count}</h1>
