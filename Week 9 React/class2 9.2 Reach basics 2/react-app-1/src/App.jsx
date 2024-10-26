eb7d052e5cb692d345ca646529859d300df16ad7
Shivamvashist <ninjavashistsv1@gmail.com>
2024-10-26 21:52:19 +0530

    Props and made the clock not loose its value by adding Props

--- Week 9 React/class2 9.2 Reach basics 2/react-app-1/src/App.jsx
+++ Week 9 React/class2 9.2 Reach basics 2/react-app-1/src/App.jsx
@@ -8,7 +8,20 @@ function App() {
 
   const [counterVisible,setCounterVisible]=useState(true);
 
+  const [count, setCount] = useState(0);
 
+  useEffect(function() {
+    console.log("on Mount")
+    let clock = setInterval(function(){
+      console.log("inside Interval")
+      setCount(count=>count+1);
+    },1000);
+  
+    return function(){
+      console.log("on unmount")
+      clearInterval(clock)
+    }
+  },[]);
 
   useEffect(function(){
     setInterval(()=>{
@@ -18,44 +31,24 @@ function App() {
 
   return <div>
     <h1>Hi There!</h1>
-    {/* {counterVisible ? <Counter></Counter> : null} */}
-  <div style={{visibility: counterVisible ? "visible" : "hidden"}} ><Counter></Counter></div>
+    {counterVisible ? <Counter count={count} ></Counter> : null}
   </div>
+
 
 
 }
 
-function Counter() {
-
-  const [count, setCount] = useState(0);
-  
+function Counter(props) {
 
   function onClickFn(){
-    setCount(count + 1);
+    setCount(props.count + 1);
   }
 
-  useEffect(function() {
-    console.log("on Mount")
-    let clock = setInterval(function(){
-      console.log("inside Interval")
-      setCount(count=>count+1);
-    },1000);
-
-    return function(){
-      console.log("on unmount")
-      clearInterval(clock)
-    }
-
-  },[]);
-
   return<div> 
-  <h1>{count}</h1>
+  <h1>{props.count}</h1>
   <button onClick={onClickFn}>Counter</button>
   </div>
-
-
-  
-  
+ 
 }
 
 export default App
