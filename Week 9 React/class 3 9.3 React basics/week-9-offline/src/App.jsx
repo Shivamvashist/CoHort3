1b3bf94de1f8aff1a42f6011ce5796068cff91ec
Shivamvashist <ninjavashistsv1@gmail.com>
2024-11-06 14:57:17 +0530

    conditional rendering with toggle button

--- Week 9 React/class 3 9.3 React basics/week-9-offline/src/App.jsx
+++ Week 9 React/class 3 9.3 React basics/week-9-offline/src/App.jsx
@@ -7,28 +7,33 @@ function App() {
       <div style={{display:"flex",justifyContent:"center",flexDirection:"column",alignItems:"center",gap:20}}>
           <Postcomponent 
             name={"Shivam"}
+            img={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREFJYOA4MyzlZSghQSHhkxQx03MUJXMykmrw&s"}
             subtitle={"20k followers"}
-            time={"2m ago"}
+            time={"5m ago"}
             description={"Hi! this is my first post"}
           />
           
           <Postcomponent 
-            name={"Shivam"}
+            name={"Harkirat"}
+            img={"https://appx-wsb-gcp.akamai.net.in/subject/2023-01-17-0.17044360120951185.jpg"}
             subtitle={"Promoted"}
             description={"This is a promoted post that has no time and promoted written instead of followers count!"}
           />
 
           <Postcomponent 
             name={"Shivam"}
+            img={"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcREFJYOA4MyzlZSghQSHhkxQx03MUJXMykmrw&s"}
             subtitle={"20k followers"}
             time={"3m ago"}
             description={"Alod when aeew do not do anything so we not to do it and lets d ti  asdn and also mnake a sandwhicth"}
           />
       </div>
+      <ToggleMessage />
     </div>
 
   )
 }
+
 
 function Postcomponent(props){
   return <div style={{
@@ -39,7 +44,7 @@ function Postcomponent(props){
     padding:20,
   }}>
     <div style={{display:"flex",gap:16,fontSize:14}}>
-      <img src={"https://appx-wsb-gcp.akamai.net.in/subject/2023-01-17-0.17044360120951185.jpg"} style={{
+      <img src={props.img} style={{
         width:40,height:40,borderRadius:20
       }}/>
       <div>
@@ -53,6 +58,21 @@ function Postcomponent(props){
     <br></br>
     <div>{props.description}</div>
   </div>
+}
+
+function ToggleMessage(){
+
+  const [isVisible,setVisible] = useState(false)
+
+  function toggle(){
+    setVisible(!isVisible)
+  }
+
+  return <div >
+    <button onClick={toggle} >Toggle message</button>
+    {isVisible ? <p>This is a conditionally rendered message</p> : null} 
+  </div>
+
 }
 
 export default App
