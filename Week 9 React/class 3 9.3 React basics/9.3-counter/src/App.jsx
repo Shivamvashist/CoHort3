3ab851880b01c35251e696077ad6301049346716
Shivamvashist <ninjavashistsv1@gmail.com>
2024-11-06 22:20:28 +0530

    dependency array in use

--- Week 9 React/class 3 9.3 React basics/9.3-counter/src/App.jsx
+++ Week 9 React/class 3 9.3 React basics/9.3-counter/src/App.jsx
@@ -3,14 +3,22 @@ import { useState,useEffect } from 'react'
 
 function App() {
   const [count,setCount] = useState(0);
-
+  const [color,setColor] = useState(true);
+  
   useEffect(()=>{
     setInterval(()=>{
       setCount(c => c+1)
     },1000)
   },[])
 
-  return <div>
+  useEffect(()=>{
+    setColor(!color)
+  },[count])
+
+  let bgclr = "yellow";
+  {color ? bgclr="green" : bgclr="red"}
+
+  return <div style={{height:36,width:40,color:'white',display:'flex',justifyContent:"center",alignItems:"center",borderRadius:20,backgroundColor:bgclr}}>
     {count}
   </div>
 
