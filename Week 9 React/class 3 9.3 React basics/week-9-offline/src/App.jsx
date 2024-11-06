5d172f6ec378df88a4481c8bbb7241abb0bb1964
Shivamvashist <ninjavashistsv1@gmail.com>
2024-11-06 13:50:26 +0530

    conditional rendering and props

--- Week 9 React/class 3 9.3 React basics/week-9-offline/src/App.jsx
+++ Week 9 React/class 3 9.3 React basics/week-9-offline/src/App.jsx
@@ -5,34 +5,53 @@ function App() {
   return (
     <div style={{backgroundColor:"#dfe6e9",height:"100vh"}}>
       <div style={{display:"flex",justifyContent:"center",flexDirection:"column",alignItems:"center",gap:20}}>
-          <Postcomponent />
-          <Postcomponent />
-          <Postcomponent />
+          <Postcomponent 
+            name={"Shivam"}
+            subtitle={"20k followers"}
+            time={"2m ago"}
+            description={"Hi! this is my first post"}
+          />
+          
+          <Postcomponent 
+            name={"Shivam"}
+            subtitle={"Promoted"}
+            description={"This is a promoted post that has no time and promoted written instead of followers count!"}
+          />
+
+          <Postcomponent 
+            name={"Shivam"}
+            subtitle={"20k followers"}
+            time={"3m ago"}
+            description={"Alod when aeew do not do anything so we not to do it and lets d ti  asdn and also mnake a sandwhicth"}
+          />
       </div>
     </div>
 
   )
 }
 
-function Postcomponent(){
+function Postcomponent(props){
   return <div style={{
     width:300,
     borderRadius:10,
-    border:"1px solid black",
+    border:"1px solid gray",
     backgroundColor: "white",
-    padding:20
+    padding:20,
   }}>
-    <div style={{display:"flex",gap:16}}>
+    <div style={{display:"flex",gap:16,fontSize:14}}>
       <img src={"https://appx-wsb-gcp.akamai.net.in/subject/2023-01-17-0.17044360120951185.jpg"} style={{
         width:40,height:40,borderRadius:20
       }}/>
       <div>
-        <b>100xdevs</b>
-        <div>23,888 followers</div>
-        <div>12m</div>
+        <b>{props.name}</b>
+        <div>{props.subtitle}</div>
+        {/* CONDITIONAL RENDERING */}
+        {props.time ? <div>{props.time} <img style={{width:10,paddingLeft:2}} src="https://www.svgrepo.com/show/6230/clock.svg" /></div> : null}
+        {/* or just {props.time && <div>time nd icon</div>} */}
       </div>
     </div>
-    <div>What to know and how to win big!</div>
+    <br></br>
+    <div>{props.description}</div>
   </div>
 }
 
