a8312f68f40d9956d567cb2e85daa764e155c248
Shivamvashist <ninjavashistsv1@gmail.com>
2024-11-07 11:18:39 +0530

    fetching api with useEffect conditionally

--- Week 9 React/class 3 9.3 React basics/fetch-todo/src/App.jsx
+++ Week 9 React/class 3 9.3 React basics/fetch-todo/src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState,useEffect } from 'react'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [currentTab, setCurrentTab] = useState(0)
+  const [ tabData,setTabData ] = useState({})
+  const [load,setLoad] =useState(true)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  let style = {backgroundColor:"red",padding:"10px 20px",borderRadius:10};
+
+  useEffect(()=>{
+    console.log(`fetch req on btn ${currentTab}`)
+
+    setLoad(true)
+
+    fetch(`https://jsonplaceholder.typicode.com/todos/${currentTab}`)
+    .then(async res => {
+      const data = await res.json()
+      setTabData(data)
+      setLoad(false)
+    })
+
+    // .then(res=>res.json()).then(json=>setTabData(json))
+
+  },[currentTab])
+
+  return <div>
+
+    <button onClick={()=>{setCurrentTab(1)}} style={currentTab == 1 ? style :{}} >Todo 1</button>
+    <button onClick={()=>{setCurrentTab(2)}} style={currentTab == 2 ? style :{}} >Todo 2</button>
+    <button onClick={()=>{setCurrentTab(3)}} style={currentTab == 3 ? style :{}} >Todo 3</button>
+    <button onClick={()=>{setCurrentTab(4)}} style={currentTab == 4 ? style :{}} >Todo 4</button>
+    <br></br>
+    <br></br>
+    {load ? "Loading..." : tabData.title }
+    <br></br>
+  </div>
 }
 
 export default App
