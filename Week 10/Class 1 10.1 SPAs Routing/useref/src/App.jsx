ca0237a50fe174a72d48eef029f089d824a8ba9e
Shivamvashist <ninjavashistsv1@gmail.com>
2024-11-08 23:19:31 +0530

    Using UseRef and useState, made a clock with start stop button

--- Week 10/Class 1 10.1 SPAs Routing/useref/src/App.jsx
+++ Week 10/Class 1 10.1 SPAs Routing/useref/src/App.jsx
@@ -1,7 +1,48 @@
 import { useState,useEffect,useRef } from 'react'
 import {BrowserRouter,Routes,Route,Outlet,Link} from 'react-router-dom'
 
+
+
+//BUILDING a clock with a start and stop button;
+
+
 function App() {
+
+  const [currentCount,setCurrentCount] = useState(1);
+
+  // const [timer,setTimer]=useState(0);
+// We will use useRef here instead of useState to prevent extra rerender and also this state is useless as no where shown in html
+
+const timer = useRef()
+
+  function onStart(){
+    let value = setInterval(()=>{
+      setCurrentCount(c=>c+1)
+    },1000)
+    timer.current=value
+  }
+
+  function onStop(){
+    clearInterval(timer.current);
+  }
+
+  return <div>
+    <Signup />
+
+    <div style={{padding:10}}>
+      <button onClick={onStart}  style={{margin:10}}>Start Clock</button>
+      {currentCount}
+      <button onClick={onStop} style={{margin:10}}>Stop Clock</button>
+    </div>
+
+
+  </div>
+}
+
+
+
+
+function Signup() {
 
   const inputRef = useRef();
 
@@ -12,7 +53,4 @@ function App() {
     <button onClick={()=>{inputRef.current.focus()}}>Submit</button>
   </div>
 }
-
-
-
 export default App
