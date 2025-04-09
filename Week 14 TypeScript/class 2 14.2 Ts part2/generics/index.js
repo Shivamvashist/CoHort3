a9124d3c25ca588c6790329bd87b70a130479d69
Shivamvashist <ninjavashistsv1@gmail.com>
2025-04-09 11:49:07 +0530

    Generics in ts

---
+++ Week 14 TypeScript/class 2 14.2 Ts part2/generics/index.js
@@ -0,0 +1,14 @@
+"use strict";
+//Generics allow u to create components that work with any data type while still providing compile time type safety
+function getFirstEle(arr) {
+    return arr[2];
+}
+let arr1 = [1, 2, 3];
+let arr2 = ["abcx", "abbd", 'ASdds'];
+let arr3 = [1, 2, "abbd", 'ASdds'];
+let a = getFirstEle(arr1);
+let b = getFirstEle(arr2);
+let c = getFirstEle(arr3);
+console.log(a);
+console.log(b);
+console.log(c.toUpperCase());
